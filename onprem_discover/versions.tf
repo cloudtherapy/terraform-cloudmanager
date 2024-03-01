22489ad905784a8c3fb601d9d7cd8925cbb5d280
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2024-02-29 21:25:37 -0500

    Updated provider version to "24.2.0"

--- onprem_discover/versions.tf
+++ onprem_discover/versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
-    # https://registry.terraform.io/providers/NetApp/netapp-cloudmanager/latest/docs
     netapp-cloudmanager = {
       source  = "NetApp/netapp-cloudmanager"
-      version = "22.4.0"
+      version = "24.2.0"
     }
   }
 }
